5f8ce3a854ce5f5637ab8ab6600ca0fda2d48c43
fikra1209 <94928013+fikra1209@users.noreply.github.com>
2026-06-21 16:10:48 +0700

    Update Loader-LuxvS.lua

--- Loader-LuxvS.lua
+++ Loader-LuxvS.lua
@@ -7,6 +7,7 @@ local Games = {
     [70845479499574]  = "https://raw.githubusercontent.com/louissxe/Lua/main/keyless/bitebynight.lua",
     [130274245431977] = "https://raw.githubusercontent.com/louissxe/Lua/main/keyless/ClimbAndPlunge.lua",
     [117381420723145] = "https://raw.githubusercontent.com/fikra1209/Lua/main/keyless/summon_heroes.lua", -- Lobby SH
+    [93978595733734]  = "https://raw.githubusercontent.com/fikra1209/Lua/main/keyless/violence_district.lua", -- Violence District
 }
 
 -- Deteksi pintar jika masuk arena battle Summon Heroes
